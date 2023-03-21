bad144342269cc78fb121d4bb03d6dad60e37e19
chloebang <126551420+chloebang@users.noreply.github.com>
2023-03-21 14:45:21 +0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,17 @@
+function myFunction() {
+  var num1 = document.getElementById("num1");
+  var num2 = document.getElementById("num2");
+  var operator = document.getElementByTagName("button");
+  let result;
+  switch (getElementByTagName.getElementById("button")){
+    case 0:
+      result = num1 + num2;
+    case (operator == getElementById("sub"));
+      result = num1 - num2;
+    case (operator == getElementById"mul"))
+      result = num1 * num2;
+    case (operator == getElementByTagName("div"))
+      result = num1 / num2;
+}
+
+document.getElementById("demo").innerHTML = result;
